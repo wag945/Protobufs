74da5eda2de4cc12b08e4a5e736f79f5d4932e83
Bill Ganley <wag945@gmail.com>
2021-07-27 19:48:22 -0400

    Build my-app with protobufs

--- Java/server/my-app/src/main/java/com/mycompany/app/App.java
+++ Java/server/my-app/src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,64 @@
 package com.mycompany.app;
 
-/**
- * Hello world!
- *
- */
+import java.io.IOException;
+import java.net.StandardProtocolFamily;
+import java.net.UnixDomainSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Optional;
+//import com.mycompany.app.PlayersOuterClass.Players;
+//import PlayersOuterClass.Players;
+
+import com.mycompany.app.PlayersOuterClass.Players;
+
 public class App 
 {
-    public static void main( String[] args )
+    private static void readAndPrintMessages(SocketChannel channel) throws IOException, InterruptedException
     {
-        System.out.println( "Hello World!" );
+        while(true)
+        {
+            readMessageFromSocket(channel).ifPresent(System.out::println);
+            Thread.sleep(100);
+        }
+    }
+
+    private static Optional<String> readMessageFromSocket(SocketChannel channel) throws IOException
+    {
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        int bytesRead = channel.read(buffer);
+        System.out.println("bytesRead: " + bytesRead);
+        if (bytesRead < 0)
+        {
+            return Optional.empty();
+        }
+        byte[] bytes = new byte[bytesRead];
+        buffer.flip();
+        buffer.get(bytes);
+        String message = new String(bytes);
+        PlayersOuterClass.Players players = Players.parseFrom(bytes);
+        PlayersOuterClass.Player player = players.getPlayerList(0);
+        System.out.println(player.toString());
+        return Optional.of(message);
+    }
+
+    public static void main(String[] args)
+        throws IOException, InterruptedException
+    {
+        Path socketFile = Path.of("/tmp").resolve("PlayersServer");
+        //Files.deleteIfExists(socketFile);
+        
+        UnixDomainSocketAddress address = UnixDomainSocketAddress.of(socketFile);
+
+        ServerSocketChannel serverChannel = ServerSocketChannel.open(StandardProtocolFamily.UNIX);
+        serverChannel.bind(address);
+
+        System.out.println("[INFO] Waiting for client to connect...");
+        SocketChannel channel = serverChannel.accept();
+        System.out.println("[INFO] Client connected");
+
+        readAndPrintMessages(channel);
     }
 }
